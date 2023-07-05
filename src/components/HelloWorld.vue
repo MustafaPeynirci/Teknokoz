9d3a15e766a822b5a6efde6151b0421656755076
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-07-05 18:44:49 +0300

    First

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+        <h2 class="text-muted fw-bold" href="#">TEKNOKOZ</h2>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Biz Kimiz</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Hakkımızda</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link" href="#">İletişim</a>
+          </li>
+          <button type="button" class="btn btn-outline-secondary border border-secondary rounded-pill">Destek</button>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container mt-10 mb-5">
+    <div class="row text-start">
+      <div class="col-md-4">
+        <img src="../assets/img/1.jpg" width="430" alt="teknokoz">
+      </div>
+      <div class="col-md-8 ps-5 pt-100">
+        <h1 class="fw-bolder mb-0">Bi Tık Daha Fazlası!</h1>
+        <span class="fw-medium fs-4">Teknokoz'da</span>
+        <p class="mt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae,
+          feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non
+          eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus
+          maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat.
+        </p>
+        <button type="button" class="btn btn-secondary border border-secondary rounded-pill fs-14 purple">Hemen Bilgi Alın</button>
+      </div>
   </div>
+  <h6 class="mt-15">Aşağı Kaydır ↓</h6>
+
+  <div class="row text-start mt-10">
+      <div class="col-md-8 pe-5 pt-100">
+        <span class="fw-medium fs-4">Tecrübeyle Sabit</span>
+        <h1 class="fw-bolder ">Kusursuz Performans</h1>
+        <p class="mt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae,
+          feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non
+          eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus
+          maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat.
+        </p>
+      </div>
+      <div class="col-md-4">
+        <img src="../assets/img/2.png" width="430" alt="teknokoz">
+        </div>
+        <div class="d-flex mt-10 text-webkit-center">
+          <div class="">
+            <img src="../assets/logo.png" width="100" alt="..." class="rounded-circle">
+            <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+          <div>
+            <img src="../assets/logo.png" width="100" alt="..." class="rounded-circle">
+            <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+          <div>
+            <img src="../assets/logo.png" width="100" alt="..." class="rounded-circle">
+            <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+          <div>
+            <img src="../assets/logo.png" width="100" alt="..." class="rounded-circle">
+            <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+        </div>
+  </div>
+
+  <div class="row text-start mt-10">
+      <div class="col-md-4">
+        <img src="../assets/img/3.png" width="430" alt="teknokoz">
+      </div>
+      <div class="col-md-8 ps-5 pt-100">
+        <span class="fw-medium fs-4">Yüksek Grafikler</span>
+        <h1 class="fw-bolder ">Mutlu Müşteriler</h1>
+        <p class="mt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae,
+          feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non
+          eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus
+          maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat.
+        </p>
+      </div>
+  </div>
+
+  <div class="row text-start mt-10">
+      <div class="col-md-8 pe-5 pt-100">
+        <span class="fw-medium fs-4">Kullanıcı Deneyimi</span>
+        <h1 class="fw-bolder ">Başarılı Portföy</h1>
+        <p class="mt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae,
+          feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non
+          eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus
+          maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat.
+        </p>
+      </div>
+      <div class="col-md-4">
+        <img src="../assets/img/4.png" width="430" alt="teknokoz">
+        </div>
+  </div>
+
+
+
+
+  </div>
+
 </template>
 
 <script>
@@ -37,7 +122,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -48,9 +132,24 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 18px;
 }
-a {
-  color: #42b983;
+.mt-10 {
+  margin-top: 10%;
+}
+.mt-15 {
+  margin-top: 15%;
+}
+.pt-100 {
+  padding-top: 100px;
+}
+.fs-14 {
+  font-size: 14px;
+}
+.purple {
+  background-color: rgb(111 133 244);
+}
+.text-webkit-center {
+  text-align: -webkit-center;
 }
 </style>
