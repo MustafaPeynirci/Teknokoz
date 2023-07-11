5aa2db5eb69a5d55c566a6e2bc2ab111dabae53e
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-07-11 17:52:06 +0300

    responsive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
     <div class="container-fluid">
-      <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
-        <h2 class="text-muted fw-bold" href="#">TEKNOKOZ</h2>
+      <a class="navbar-brand text-muted fw-bold fs-3 b-resp" href="#">TEKNOKOZ</a>
+      <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+        <h2 class="text-muted fw-bold s-resp" href="#">TEKNOKOZ</h2>
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
@@ -13,7 +17,7 @@
           <li class="nav-item">
             <a class="nav-link" href="#">Hakkımızda</a>
           </li>
-          <li class="nav-item me-5">
+          <li class="nav-item">
             <a class="nav-link" href="#">İletişim</a>
           </li>
           <button type="button" class="btn btn-outline-secondary border border-secondary rounded-pill">Destek</button>
@@ -23,41 +27,41 @@
   </nav>
   <div class="container mt-10 mb-5">
     <div class="row text-start">
-      <div class="col-md-4">
-        <img src="../assets/img/1.jpg" width="430" alt="teknokoz">
-      </div>
-      <div class="col-md-8 ps-5 pt-100">
-        <h1 class="fw-bolder mb-0">Bi Tık Daha Fazlası!</h1>
-        <span class="fw-medium fs-4">Teknokoz'da</span>
-        <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat. </p>
-        <button type="button" class="btn btn-secondary border border-secondary rounded-pill fs-14 purple">Hemen Bilgi Alın</button>
+        <div class="col-md-5">
+          <img src="../assets/img/1.png" class="w-100" alt="teknokoz">
+        </div>
+        <div class="col-md-7 pt-100">
+          <h1 class="fw-bolder mb-0">Bi Tık Daha Fazlası!</h1>
+          <span class="fw-medium fs-4">Teknokoz'da</span>
+          <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat. </p>
+          <button type="button" class="btn btn-secondary border border-secondary rounded-pill h-zoom fs-14 purple">Hemen Bilgi Alın</button>
       </div>
     </div>
     <h6 class="mt-15">Aşağı Kaydır ↓</h6>
     <div class="row text-start mt-10">
-      <div class="col-md-8 pe-5 pt-100">
+      <div class="col-md-7 pe-5 pt-100">
         <span class="fw-medium fs-4">Tecrübeyle Sabit</span>
         <h1 class="fw-bolder ">Kusursuz <span class="red-1">Performans</span>
         </h1>
         <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat. </p>
       </div>
-      <div class="col-md-4">
-        <img src="../assets/img/2.png" width="430" alt="teknokoz">
+      <div class="col-md-5">
+        <img src="../assets/img/2.png" class="w-100" alt="teknokoz">
       </div>
-      <div class="d-flex mt-10 text-webkit-center">
-        <div class="">
+      <div class="d-flex mt-10 text-webkit-center display-resp">
+        <div class="h-zoom">
           <i class="fa-solid fa-file fa-3x mb-4 text-body"></i>
           <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
         </div>
-        <div>
+        <div class="h-zoom">
           <i class="fa-solid fa-code fa-3x mb-4 text-body"></i>
           <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
         </div>
-        <div>
+        <div class="h-zoom">
           <i class="fa-solid fa-gear fa-3x mb-4 text-body"></i>
           <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
         </div>
-        <div>
+        <div class="h-zoom">
           <i class="fa-solid fa-pen-to-square fa-3x mb-4 text-body"></i>
           <p class="w-75">Lorem ipsum dolor sit amet consectetur adipisicing </p>
         </div>
@@ -65,7 +69,7 @@
     </div>
     <div class="row text-start mt-10">
       <div class="col-md-4">
-        <img src="../assets/img/3.png" width="350" alt="teknokoz">
+        <img src="../assets/img/3.png" class="w-100" alt="teknokoz">
       </div>
       <div class="col-md-8 ps-5 pt-100">
         <span class="fw-medium fs-4">Yüksek Grafikler</span>
@@ -73,38 +77,44 @@
           <span class="text-warning">Mutlu</span> Müşteriler
         </h1>
         <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat. </p>
-        <div class="d-flex g-40 mt-5">
+        <div class="d-flex g-40 mt-5 display-resp">
           <div>
             <i class="fa-solid fa-people-group fa-2x mb-2 purple-2"></i>
-            <h2 class="fw-semibold">500+</h2>
-            <span>Mutlu Müşteri</span>
+            <div class="h-zoom">
+              <h2 class="fw-semibold">500+</h2>
+              <span>Mutlu Müşteri</span>
+            </div>
           </div>
           <div>
             <i class="fa-solid fa-thumbs-up fa-2x mb-2 purple-2"></i>
-            <h2 class="fw-semibold">1027+</h2>
-            <span>Tavsiye Edildi</span>
+            <div class="h-zoom">
+              <h2 class="fw-semibold">1027+</h2>
+              <span>Tavsiye Edildi</span>
+            </div>
           </div>
           <div>
             <i class="fa-solid fa-list fa-2x mb-2 purple-2"></i>
-            <h2 class="fw-semibold">243+</h2>
-            <span>Çözümlenen Task</span>
+            <div class="h-zoom">
+              <h2 class="fw-semibold">243+</h2>
+              <span>Çözümlenen Task</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div class="row text-start mt-10">
-      <div class="col-md-8 pe-5 pt-100">
+      <div class="col-md-7 pe-5 pt-100">
         <span class="fw-medium fs-4">Kullanıcı Deneyimi</span>
         <h1 class="fw-bolder ">Başarılı <span class="text-success">Portföy</span>
         </h1>
         <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum. Vivamus tincidunt dignissim magna, non eleifend mauris viverra in. Nam suscipit neque ex, vitae accumsan urna consequat ut. Vivamus maximus fringilla est ut aliquet. Phasellus aliquam nec odio vel placerat. </p>
       </div>
-      <div class="col-md-4">
-        <img src="../assets/img/4.png" width="430" alt="teknokoz">
+      <div class="col-md-5">
+        <img src="../assets/img/4.png" class="w-100" alt="teknokoz">
       </div>
     </div>
   </div>
-  <div class="bg-pgradient mt-10">
+  <div class="bg-pgradient mt-10 display-resp-none">
     <div class="col-md-4 mx-auto">
       <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
@@ -160,7 +170,7 @@
     <span class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
     <div class="row row-cols-1 row-cols-md-3 g-5 mt-4">
       <div class="col">
-        <div class="card h-100 border-0">
+        <div class="card h-zoom h-100 border-0">
           <img src="../assets/img/blog-1.jpg" class="card-img-top" alt="teknokoz">
           <div class="card-body text-start grey">
             <h5 class="">Kurumsal Web Sitesi</h5>
@@ -170,7 +180,7 @@
         </div>
       </div>
       <div class="col">
-        <div class="card h-100 border-0">
+        <div class="card h-zoom h-100 border-0">
           <img src="../assets/img/blog-2.jpg" class="card-img-top" alt="teknokoz">
           <div class="card-body text-start grey">
             <h5 class="">Özel Yazılım</h5>
@@ -180,7 +190,7 @@
         </div>
       </div>
       <div class="col">
-        <div class="card h-100 border-0">
+        <div class="card h-zoom h-100 border-0">
           <img src="../assets/img/blog-3.jpg" class="card-img-top" alt="teknokoz">
           <div class="card-body text-start grey">
             <h5 class="">Dijital Market</h5>
@@ -192,12 +202,14 @@
     </div>
   </div>
   <div class="bg-pgradient-2 mt-10 mb-5">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1131.0135113063527!2d27.12926805229604!3d38.42011320048231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bbd8e3ac245af7%3A0x3585bde806b7c8e!2sKonak%2C%20Cumhuriyet%20Blv%20No%3A20%2C%2035251%20Konak%2F%C4%B0zmir!5e0!3m2!1str!2str!4v1688907105714!5m2!1str!2str" width="750" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="mt-6 rounded-3"></iframe>
     <div class="container">
-      <div class="row mt-5 text-start g-100">
+      <div class="col-md-8 mx-auto">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1131.0135113063527!2d27.12926805229604!3d38.42011320048231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bbd8e3ac245af7%3A0x3585bde806b7c8e!2sKonak%2C%20Cumhuriyet%20Blv%20No%3A20%2C%2035251%20Konak%2F%C4%B0zmir!5e0!3m2!1str!2str!4v1688907105714!5m2!1str!2str" width="100%" height="450px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="mt-6 rounded-3"></iframe>
+      </div>
+      <div class="row mt-5 text-start">
         <div class="col-md-6">
           <h1 class="fw-bold">Bizimle İletişime Geçin</h1>
-          <div class="col-md-12 d-flex">
+          <div class="col-md-12 d-flex display-resp">
             <div class="col-md-5 mt-4 d-flex flex-column g-20">
               <div class="input-group flex-nowrap">
                 <input type="text" class="form-control" placeholder="Ad Soyad" aria-label="adSoyad" aria-describedby="addon-wrapping">
@@ -209,7 +221,7 @@
                 <input type="text" class="form-control" placeholder="Konu" aria-label="konu" aria-describedby="addon-wrapping">
               </div>
             </div>
-            <div class="col-md-7 mt-4 ps-4">
+            <div class="col-md-7 mt-4 ps-4 padding-resp">
               <div class="form-floating h-100">
                 <textarea class="form-control h-100" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
                 <label for="floatingTextarea2">Mesaj</label>
@@ -217,10 +229,10 @@
             </div>
           </div>
           <div class="mt-3">
-            <button type="button" class="btn btn-secondary border border-secondary rounded-pill fs-14 purple w-100">Gönder</button>
+            <button type="button" class="btn btn-secondary border border-secondary rounded-pill h-zoom fs-14 purple w-100">Gönder</button>
           </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-6 ps-5 margin-resp">
           <h1 class="fw-bold">Adres</h1>
           <div class="row mt-4 text-start">
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo neque, suscipit nec ligula vitae, feugiat venenatis tellus. In molestie ut massa quis dictum.</p>
@@ -236,7 +248,7 @@
         </div>
       </div>
     </div>
-    <div class="container mt-6">
+    <div class="container mt-6 margin-resp">
       <span class="pb-4">© 2023 Teknokoz. Tüm Hakları Saklıdır.</span>
     </div>
   </div>
@@ -262,6 +274,46 @@
   li {
     display: inline-block;
     margin: 0 18px;
+  }
+
+  .s-resp {
+    @media only screen and (max-width: 992px) {
+      display: none;
+    }
+  }
+
+  .b-resp {
+    @media only screen and (min-width: 992px) {
+      display: none;
+    }
+  }
+
+  .display-resp {
+    @media only screen and (max-width: 768px) {
+      display: block !important;
+    }
+  }
+
+  .display-resp-none {
+    @media only screen and (max-width: 768px) {
+      display: none !important;
+    }
+  }
+  
+  .padding-resp {
+    @media only screen and (max-width: 768px) {
+      padding-left: 0 !important;
+    }
+  }
+
+  .margin-resp {
+    @media only screen and (max-width: 768px) {
+      margin-top: 50px !important;
+    }
+  }
+
+  .navbar-toggler:focus {
+    box-shadow: none;
   }
 
   .mt-6 {
@@ -316,10 +368,6 @@
     gap: 40px;
   }
 
-  .g-100 {
-    gap: 100px;
-  }
-
   .bg-pgradient {
     background-image: url("../assets/img/gradient.png");
     height: 560px;
@@ -330,5 +378,13 @@
     background-image: url("../assets/img/gradient-2.png");
     height: 380px;
     background-size: cover;
+  }
+
+  .h-zoom {
+    transition: transform .3s;
+  }
+
+  .h-zoom:hover {
+    transform: scale(1.05);
   }
 </style>
